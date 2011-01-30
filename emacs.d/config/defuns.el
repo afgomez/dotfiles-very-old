b0f3d7d85527066857566976d6e837a4720fed76
Alejandro Fernández <antarticonorte@gmail.com>
2011-01-30 21:09:11 +0100

    Cut/copy current line if nothing is selected

--- emacs.d/config/defuns.el
+++ emacs.d/config/defuns.el
@@ -46,3 +46,20 @@
 (defun turn-off-tool-bar ()
   (tool-bar-mode -1))
 
+
+
+;; If nothing is selected, kill/copy current line
+
+
+(defadvice kill-ring-save (before slick-copy activate compile) "When called
+  interactively with no active region, copy a single line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end)) (message
+  "Copied line") (list (line-beginning-position) (line-beginning-position
+  2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))

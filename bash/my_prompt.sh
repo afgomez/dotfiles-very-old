75896d6a04122349c7454f703810083edc73a266
Victor Ortiz <kespers@gmail.com>
2011-09-04 05:27:21 +0200

    Fix del prompt de bash
    Merged pull request from https://github.com/afgomez/dotfiles/pull/1

---
+++ bash/my_prompt.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+	
+status="\w"
+
+### Ruby version & gemset colorized
+gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
+ 
+if [[ "$gemset" != "global" ]]; then
+  gemset="@$gemset"
+else
+  gemset=""
+fi
+
+version=$(echo $MY_RUBY_HOME | awk -F'-' '{print $2}')
+
+[ "$version" == "1.8.7" ] && version=""
+full="$version$gemset"
+if [ "$full" != "" ]; then
+	status="${status}[\[\033[0;35m\]${full}\[\033[0;0m\]]"
+fi
+
+
+### Git branchs colorized
+branch=$(__git_ps1 "%s")
+ if [ -n "$branch" ]; then
+   color="\033[0;33m" # blue
+   
+   if [[ $branch == master* ]]; then
+     color="\033[0;31m" # Red
+   fi
+   if [[ $branch == dev* ]]; then
+     color="\033[0;32m" # Green
+   fi
+   if [[ $branch == features* ]]; then
+     color="\033[0;36m" # Cyan
+   fi
+   if [[ $branch == fix* ]]; then
+     color="\033[0;35m" # Light Red
+   fi
+   branch=$(printf "${branch}")
+   color=$(printf "${color}")
+
+   status="${status}[\[${color}\]${branch}\[\033[0;0m\]]\$ "
+else
+  status="${status}\$ "
+fi
+
+echo "$status"

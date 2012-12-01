2e7f8d313c144e4003dbebef7a12039c01346f67
Alejandro Fernández <antarticonorte@gmail.com>
2012-12-01 02:05:34 +0100

    Remove RVM info from the prompt
    I'm planning to use rbenv exclusively so I don't need RVM info
    anymore. Let that information empty until I figure out a good way to
    do the same using rbenv

--- bash/my_prompt.sh
+++ bash/my_prompt.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-	
+
 status="\w"
-
-### Ruby version & gemset colorized
-gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
- 
-if [[ "$gemset" != "global" ]]; then
-  gemset="@$gemset"
-else
-  gemset=""
-fi
-
-version=$(echo $MY_RUBY_HOME | awk -F'-' '{print $2}')
-
-[ "$version" == "1.8.7" ] && version=""
-full="$version$gemset"
-if [ "$full" != "" ]; then
-	status="${status}[\[\033[0;35m\]${full}\[\033[0;0m\]]"
-fi
-
 
 ### Git branchs colorized
 branch=$(__git_ps1 "%s")
  if [ -n "$branch" ]; then
    color="\033[0;33m" # blue
-   
+
    if [[ $branch == master* ]]; then
      color="\033[0;31m" # Red
    fi

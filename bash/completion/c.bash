f9a70061ba3397016a2c3893dd7e0df98901213c
Alejandro Fernández Gómez <a.fernandez.gomez@catawiki.nl>
2016-09-20 11:56:27 +0200

    Allow to set up different `CODE_PATH`s per machine
    By default the `c` command will expect your code to live in `~/code`,
    but you can specify your own `CODE_PATH` in the machine `~/.localrc`

--- bash/completion/c.bash
+++ bash/completion/c.bash
@@ -1,14 +1,15 @@
 # Provides autocompletion for the c command
 function _c {
+  CODE_PATH="${CODE_PATH:-~/code}"
   local cur prev opts
   COMREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  directories="$(command ls -d ~/code/*/ | awk -F"/"  '{ print  $(NF-1) }' | tr '\n' ' ')"
-  
+  directories="$(command ls -d $CODE_PATH/*/ | awk -F"/"  '{ print  $(NF-1) }' | tr '\n' ' ')"
+
   COMPREPLY=( $(compgen -W "${directories}" -- ${cur}) )
   return 0
-  
+
 }
 
 complete -F _c c
